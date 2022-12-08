b76f55f3a1b200be858c37197c883b4e62b43185
BALAVENKATTADEPALLI <115427246+BALAVENKATTADEPALLI@users.noreply.github.com>
2022-12-08 06:40:03 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
-// getUsers button 
+ 
  document.getElementById("btn-users").addEventListener('click', getUsers);
 
 function getUsers() {
- fetch("http://localhost:3000/users/")
- .then((res)=> res.json())
- .then((data) => console.log(data))
- .catch((err)=> console.log(err))
-}
+  fetch("http://localhost:3000/users/")
+  .then((res)=> res.json())
+  .then((data) => console.log(data))
+   .catch((err)=> console.log(err))
+ }
 
 
 
@@ -120,6 +120,5 @@ function page(b){
     UserN.reset();
 
 }
-
 
 
